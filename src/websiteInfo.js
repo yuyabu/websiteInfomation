174613766c12f3b08301bf7d2d7d57f1510d8160
yuyabu <yuyabushita@gmail.com>
2017-09-29 20:13:40 +0900

    replace deprecated method

--- src/websiteInfo.js
+++ src/websiteInfo.js
@@ -1,8 +1,11 @@
 
-chrome.tabs.getSelected(null, function(tab) {
-    var tabId = tab.id;
-    var tabUrl = tab.url;
-    var ipAdress = document.getElementById('main_text');
-    main_text.innerHTML = JSON.stringify(tab).replace(/,/g,"<br/>");
+
+chrome.tabs.query({
+    active: true,               // Select active tabs
+    lastFocusedWindow: true     // In the current window
+}, function(array_of_Tabs) {
+    var tab = array_of_Tabs[0];
+    var main_text= document.getElementById('main_text');
+    main_text.innerHTML = JSON.stringify(tab).replace(/,/g,",<br/>");
 
 });
